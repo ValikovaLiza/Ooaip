8640e8fa660df2992d1e19a1280d63733993c455
Elizaveta <liza5valikova@gmail.com>
2023-12-26 17:38:59 +0600

    All work

---
+++ SpaceBattle/MacroCommandStrategy.cs
@@ -0,0 +1,22 @@
+﻿using Hwdtech;
+
+namespace SpaceBattle;
+
+public class MacroCommandStrategy : IStrategy
+{
+    public object Strategy(params object[] args)
+    {
+        var key = (string)args[0];
+        var obj = (IUObject)args[1];
+
+        var dependencies = IoC.Resolve<IList<string>>("SpaceBattle.Operation." + key);
+        IList<_ICommand.ICommand> list = new List<_ICommand.ICommand>();
+
+        foreach (var d in dependencies)
+        {
+            list.Add(IoC.Resolve<_ICommand.ICommand>(d, obj));
+        }
+
+        return new MacroCommand(list);
+    }
+}
