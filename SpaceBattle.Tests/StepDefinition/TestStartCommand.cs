495dbbef4e796862f27ab9462d64dc49eb2d3f08
The flower collector under the bloody rain <61908871+ValikovaLiza@users.noreply.github.com>
2023-12-28 17:56:33 +0600

    Laba 2 (#2)
    * Changes
    
    * Changes
    
    * new branch again
    
    * Again
    
    * Repeat
    
    * yml
    
    * yml again
    
    * try
    
    * Without useless
    
    * injectable
    
    * injectable2
    
    * without
    
    * no conflicts
    
    * codecov
    
    * codecove
    
    * a
    
    * Точно нет конфликтов
    
    * cover
    
    * cover
    
    ---------
    
    Co-authored-by: Elizaveta <liza5valikova@gmail.com>

---
+++ SpaceBattle.Tests/StepDefinition/TestStartCommand.cs
@@ -0,0 +1,54 @@
+﻿namespace SpaceBattle.Tests;
+
+using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+
+public class ActionCommand : ICommand
+{
+    private readonly Action _action;
+    public ActionCommand(Action action) => _action = action;
+    public void Execute()
+    {
+        _action();
+    }
+}
+
+public class StartMoveCommandTests
+{
+    private readonly Mock<IMoveCommandStartable> _moveCommandStartableMock;
+    private readonly Mock<IUObject> _uObjectMock;
+    private readonly StartMoveCommand _startMoveCommand;
+
+    public StartMoveCommandTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+
+        _moveCommandStartableMock = new Mock<IMoveCommandStartable>();
+        _uObjectMock = new Mock<IUObject>();
+
+        _moveCommandStartableMock.Setup(m => m.UObject).Returns(_uObjectMock.Object);
+        _moveCommandStartableMock.Setup(m => m.Dict).Returns(new Dictionary<string, object>());
+
+        _startMoveCommand = new StartMoveCommand(_moveCommandStartableMock.Object);
+    }
+
+    [Fact]
+    public void Execute_RegistersTargetsAndPushesMovingCommand_WhenCalled()
+    {
+        var movingCommandMock = new Mock<ICommand>();
+        var commandMock = new Mock<ICommand>();
+        var queueMock = new Mock<IQueue>();
+        var injecMock = new Mock<ICommand>();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Moving.Commands", (object[] args) => movingCommandMock.Object).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Target.Register", (object[] args) => commandMock.Object).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Queue.Push", (object[] args) => queueMock.Object).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Commands.Injectable", (object[] args) => injecMock.Object).Execute();
+
+        _startMoveCommand.Execute();
+
+        _moveCommandStartableMock.Verify(m => m.Dict, Times.Once());
+        queueMock.Verify(q => q.Add(It.IsAny<ICommand>()), Times.Once());
+    }
+}
---
+++ SpaceBattle/IQueue.cs
@@ -0,0 +1,9 @@
+﻿using _ICommand;
+
+namespace SpaceBattle;
+
+public interface IQueue
+{
+    void Add(Hwdtech.ICommand cmd);
+    ICommand Take();
+}
