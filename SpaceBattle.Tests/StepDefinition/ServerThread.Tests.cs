a97723469d7278024e995d7532a91a25a7375eef
Elizaveta <liza5valikova@gmail.com>
2024-03-31 12:10:14 +0600

    ,KMJNHBG

--- SpaceBattle.Tests/StepDefinition/ServerThread.Tests.cs
+++ SpaceBattle.Tests/StepDefinition/ServerThread.Tests.cs
@@ -174,6 +174,7 @@ public class ServerTheardTests
 
         var st = new ServerThread(q, IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Current")));
         var uuid = IoC.Resolve<Guid>("Add Thread To HT And Get Uuid by it", st);
+        
         var st2 = new ServerThread(q, IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Current")));
         var uuid2 = IoC.Resolve<Guid>("Add Thread To HT And Get Uuid by it", st2);
 
