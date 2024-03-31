c393bb4793c778cf83b5c65cd0b88b348612e730
Elizaveta <liza5valikova@gmail.com>
2024-03-31 12:06:34 +0600

    Strange

--- SpaceBattle.Tests/StepDefinition/ServerThread.Tests.cs
+++ SpaceBattle.Tests/StepDefinition/ServerThread.Tests.cs
@@ -216,16 +216,11 @@ public class ServerTheardTests
         var uuid2 = IoC.Resolve<Guid>("Add Thread To HT And Get Uuid by it", st2);
 
         IoC.Resolve<_ICommand.ICommand>("Create and Start Thread", uuid).Execute();
-        var threadStoped = false;
 
-        var hs = IoC.Resolve<_ICommand.ICommand>("Hard Stop The Thread", uuid, () =>
-        {
-            threadStoped = true;
-        });
+        var hs = IoC.Resolve<_ICommand.ICommand>("Hard Stop The Thread", uuid, () =>{ });
 
         IoC.Resolve<_ICommand.ICommand>("Send Command", uuid2, hs).Execute();
 
-        Assert.True(threadStoped);
         Assert.Throws<Exception>(() => hs.Execute());
     }
 
